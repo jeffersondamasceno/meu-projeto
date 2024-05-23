d3b8c271997240202f8026f53612dd13640a6546
jeffersondamasceno <jeffersonw.damasceno@gmail.com>
2024-05-23 09:15:31 -0300

    feat: Aula 6

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import HelloWorld from './components/HelloWorld';
 import SayMyName from './components/SayMyName';
 import Pessoa from './components/Pessoa';
+import Frase from './components/Frase';
 
 function App() {
   
@@ -9,6 +10,8 @@ function App() {
   return (
     <div className="App">
       <h1>Testando CSS</h1>
+      <Frase />
+      <Frase />
       <SayMyName nome="Jefferson"/>
       <SayMyName nome="Matheus"/>
       <SayMyName nome={nome}/>
