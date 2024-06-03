9269778bd1d603203450159ee3e825b1b1ac4ff5
jeffersondamasceno <jeffersonw.damasceno@gmail.com>
2024-06-03 08:48:11 -0300

    feat: adição

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import HelloWorld from './components/HelloWorld';
 import SayMyName from './components/SayMyName';
 import Pessoa from './components/Pessoa';
 import Frase from './components/Frase';
+import List from './components/List';
 
 function App() {
   
@@ -20,6 +21,7 @@ function App() {
       idade="28" 
       profissao="Programador" 
       foto="https://placehold.co/150x150"/>
+      <List/>
     </div>
   );
 }
