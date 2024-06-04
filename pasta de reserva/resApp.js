4d4ebcc96474e8d35abfa5e13611ad0b1c7a3020
jeffersondamasceno <jeffersonw.damasceno@gmail.com>
2024-06-04 09:26:18 -0300

    feat: Aula 7, 8 e 9 adicionadas

---
+++ pasta de reserva/resApp.js
@@ -0,0 +1,29 @@
+// import './App.css';
+// import HelloWorld from './components/HelloWorld';
+// import SayMyName from './components/SayMyName';
+// import Pessoa from './components/Pessoa';
+// import Frase from './components/Frase';
+// import List from './components/List';
+
+// function App() {
+  
+//   const nome = "Maria"
+//   return (
+//     <div className="App">
+//       <h1>Testando CSS</h1>
+//       <Frase />
+//       <Frase />
+//       <SayMyName nome="Jefferson"/>
+//       <SayMyName nome="Matheus"/>
+//       <SayMyName nome={nome}/>
+//       <Pessoa 
+//       nome="Rodrigo" 
+//       idade="28" 
+//       profissao="Programador" 
+//       foto="https://placehold.co/150x150"/>
+//       <List/>
+//     </div>
+//   );
+// }
+
+// export default App;
--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
 import './App.css';
-import HelloWorld from './components/HelloWorld';
-import SayMyName from './components/SayMyName';
-import Pessoa from './components/Pessoa';
-import Frase from './components/Frase';
-import List from './components/List';
+import Evento from './components/Evento';
+import Form from './components/Form';
 
 function App() {
   
   const nome = "Maria"
   return (
     <div className="App">
-      <h1>Testando CSS</h1>
-      <Frase />
-      <Frase />
-      <SayMyName nome="Jefferson"/>
-      <SayMyName nome="Matheus"/>
-      <SayMyName nome={nome}/>
-      <Pessoa 
-      nome="Rodrigo" 
-      idade="28" 
-      profissao="Programador" 
-      foto="https://placehold.co/150x150"/>
-      <List/>
+      <h1>Testando Eventos</h1>
+      <Evento numero="1"/>
+      <Evento numero="2"/>
+      <Form/>
     </div>
   );
 }
